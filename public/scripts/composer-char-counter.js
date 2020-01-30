89c748a5d8e6f9f8c0e57f8ade2dd03f386cc3d4
kzcheng <kevinzifancheng@gmail.com>
2020-01-29 17:50:35 -0800

    Length counter now changes color when too long

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,5 +5,11 @@ $(document).ready(() => {
     let text = $('textarea').val();
     remainingLetterCounter = 140 - text.length;
     $("span.counter").text(`${remainingLetterCounter}`);
+    if (remainingLetterCounter >= 0) {
+      $("span.counter").css('color', '');
+    } else {
+      $("span.counter").css('color', 'red');
+    }
+    
   });
 });
